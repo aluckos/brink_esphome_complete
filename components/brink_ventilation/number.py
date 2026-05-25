027861f0f7a00d150d0594cd97158b7e14065d5a
aluckos <59487836+aluckos@users.noreply.github.com>
2026-05-25 12:29:51 +0200

    Add files via upload

--- components/brink_ventilation/number.py
+++ components/brink_ventilation/number.py
@@ -47,16 +47,16 @@ async def to_code(config):
         cg.add(parent.set_ventilation_number(var))
     elif number_type == "U1":
         var = cg.new_Pvariable(config[CONF_ID])
-        await number.register_number(var, config, min_value=0, max_value=100, step=1)
+        await number.register_number(var, config, min_value=0, max_value=100, step=10)
         cg.add(var.set_parent(parent))
         cg.add(parent.set_u1_number(var))
     elif number_type == "U2":
         var = cg.new_Pvariable(config[CONF_ID])
-        await number.register_number(var, config, min_value=0, max_value=100, step=1)
+        await number.register_number(var, config, min_value=0, max_value=100, step=10)
         cg.add(var.set_parent(parent))
         cg.add(parent.set_u2_number(var))
     elif number_type == "U3":
         var = cg.new_Pvariable(config[CONF_ID])
-        await number.register_number(var, config, min_value=0, max_value=100, step=1)
+        await number.register_number(var, config, min_value=0, max_value=100, step=10)
         cg.add(var.set_parent(parent))
         cg.add(parent.set_u3_number(var))
